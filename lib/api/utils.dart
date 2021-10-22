38665785d55cdcc6ee8f9f82ddf47bde5a0de6aa
Léo Sarochar <leo.sarochar@epitech.eu>
2021-10-22 13:37:08 +0200

    New v2 url

--- lib/api/utils.dart
+++ lib/api/utils.dart
@@ -5,17 +5,18 @@ import 'package:shared_preferences/shared_preferences.dart';
 Future<http.Response> getAPI(endPoint, v1) async {
   final prefs = await SharedPreferences.getInstance();
   var accessToken = prefs.getString('access_token');
-  var url = v1 ? redditAPIOAuthBaseURL :redditAPIOAuthBaseURL_V2;
+  var url = v1 ? redditAPIOAuthBaseURL : redditAPIOAuthBaseURL_V2;
   return http.get(Uri.parse('$url/$endPoint'), headers: {
     "Authorization": "Bearer $accessToken",
     "User-Agent": "Redditech/1.0.0 (by /u/redditech)"
   });
 }
 
-Future<http.Response> postAPI(endPoint, body) async {
+Future<http.Response> postAPI(endPoint, body, v1) async {
   final prefs = await SharedPreferences.getInstance();
   var accessToken = prefs.getString('access_token');
-  return http.post(Uri.parse('$redditAPIOAuthBaseURL/$endPoint'),
+  var url = v1 ? redditAPIOAuthBaseURL : redditAPIOAuthBaseURL_V2;
+  return http.post(Uri.parse('$url/$endPoint'),
       headers: {
         "Authorization": "Bearer $accessToken",
         "User-Agent": "Redditech/1.0.0 (by /u/redditech)"
@@ -23,10 +24,11 @@ Future<http.Response> postAPI(endPoint, body) async {
       body: body);
 }
 
-Future<http.Response> patchAPI(endPoint, body) async {
+Future<http.Response> patchAPI(endPoint, body, v1) async {
   final prefs = await SharedPreferences.getInstance();
   var accessToken = prefs.getString('access_token');
-  return http.patch(Uri.parse('$redditAPIOAuthBaseURL/$endPoint'),
+  var url = v1 ? redditAPIOAuthBaseURL : redditAPIOAuthBaseURL_V2;
+  return http.patch(Uri.parse('$url/$endPoint'),
       headers: {
         "Authorization": "Bearer $accessToken",
         "User-Agent": "Redditech/1.0.0 (by /u/redditech)"
@@ -34,10 +36,11 @@ Future<http.Response> patchAPI(endPoint, body) async {
       body: body);
 }
 
-Future<http.Response> putAPI(endPoint, body) async {
+Future<http.Response> putAPI(endPoint, body, v1) async {
   final prefs = await SharedPreferences.getInstance();
   var accessToken = prefs.getString('access_token');
-  return http.put(Uri.parse('$redditAPIOAuthBaseURL/$endPoint'),
+  var url = v1 ? redditAPIOAuthBaseURL : redditAPIOAuthBaseURL_V2;
+  return http.put(Uri.parse('$url/$endPoint'),
       headers: {
         "Authorization": "Bearer $accessToken",
         "User-Agent": "Redditech/1.0.0 (by /u/redditech)"
@@ -45,10 +48,11 @@ Future<http.Response> putAPI(endPoint, body) async {
       body: body);
 }
 
-Future<http.Response> deleteAPI(endPoint, body) async {
+Future<http.Response> deleteAPI(endPoint, body, v1) async {
   final prefs = await SharedPreferences.getInstance();
   var accessToken = prefs.getString('access_token');
-  return http.delete(Uri.parse('$redditAPIOAuthBaseURL/$endPoint'),
+  var url = v1 ? redditAPIOAuthBaseURL : redditAPIOAuthBaseURL_V2;
+  return http.delete(Uri.parse('$url/$endPoint'),
       headers: {
         "Authorization": "Bearer $accessToken",
         "User-Agent": "Redditech/1.0.0 (by /u/redditech)"
