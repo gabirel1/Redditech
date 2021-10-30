69ac9cbb72806c2251080209f43ae30e9aa26bbb
Léo Sarochar <leo.sarochar@epitech.eu>
2021-10-30 15:32:43 +0200

    Preparing pagination in list view

--- lib/widgets/subreddit_post_list.dart
+++ lib/widgets/subreddit_post_list.dart
@@ -4,9 +4,10 @@ import 'package:redditech/widgets/subreddit_post.dart';
 class SubRedditPostList extends StatefulWidget {
   final posts;
   final showSubRedditName;
+  final loadFunc;
 
   SubRedditPostList(
-      {Key? key, required this.posts, this.showSubRedditName = true})
+      {Key? key, required this.posts, this.loadFunc, this.showSubRedditName = true})
       : super(key: key);
 
   @override
@@ -14,19 +15,32 @@ class SubRedditPostList extends StatefulWidget {
 }
 
 class _SubRedditPostListState extends State<SubRedditPostList> {
+  var _controller = ScrollController();
+
   @override
   void initState() {
     super.initState();
+    _controller.addListener(() {
+      if (_controller.position.atEdge) {
+        if (_controller.position.pixels != 0) {
+          // Load new posts
+          widget.loadFunc();
+        }
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
+      controller: _controller,
       slivers: <Widget>[
         SliverList(
             delegate:
                 SliverChildBuilderDelegate((BuildContext context, int index) {
-          return SubRedditPost(data: widget.posts[index]["data"], showSubRedditName: widget.showSubRedditName);
+          return SubRedditPost(
+              data: widget.posts[index]["data"],
+              showSubRedditName: widget.showSubRedditName);
         }, childCount: widget.posts.length))
       ],
     );
